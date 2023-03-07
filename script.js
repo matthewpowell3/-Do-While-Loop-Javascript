fb9a1e9734d752c92e17219451656cc11932f4eb
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-07 00:28:29 -0500

    1 file modified

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Do-While Loop Javascript
 
 //if you change five to six it will go into a infinite loop
-var x = 5;
+var x = 10;
 do {
-  document.write('Hello my name is matthew learning javasrcipt');
+  document.write(x + ' Hello my name is matthews<br>');
   x--;
   // 5 is equal and not greater than five
 } while (x > 5); // codition
